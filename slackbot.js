c95a1f992bb78aa2aec4fa440de2d91f435e18bc
luke101308 <lukebirkett113@hotmail.com>
2018-07-02 14:28:51 +0100

    created mitchbot - which responds to positive comments with a mitch icon.

---
+++ slackbot.js
@@ -0,0 +1,29 @@
+const SlackBot = require('slackbots');
+ 
+// create a bot
+const bot = new SlackBot({
+    token: 'xoxb-392278351014-391287174819-afTVxkACLNXIbF0vGjmJPYnx', // Add a bot https://my.slack.com/services/new/bot and put the token 
+    name: 'MitchBot'
+});
+ 
+bot.on('message', function(data) {
+    // more information about additional params https://api.slack.com/methods/chat.postMessage
+    const params = {
+        icon_emoji: ':mitch-maths:'
+    };
+    if (data.username !== "MitchBot" && data.type === "message"){
+        const dataPositive = data.text
+        .split(' ')
+        .filter(word => 
+            word === "yes" ||
+            word === "great" ||
+            word === "cool" ||
+            word === "amazing" ||
+            word === "brilliant" ||
+            word === "bam")
+        .length
+        if (dataPositive > 0){
+            bot.postMessageToChannel('general', ':mitch-1:', params);
+        }
+    }
+});
